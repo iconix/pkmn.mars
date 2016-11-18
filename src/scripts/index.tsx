94577f8adbaff819d99251c8de4fc29581e109a9
iconix <narho@microsoft.com>
2016-11-17 22:20:25 -0800

    Introduce Typescript, Webpack, React

---
+++ src/scripts/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { Screen } from "./components/screen";
+
+ReactDOM.render(
+    <Screen />,
+    document.getElementById("screen")
+);
