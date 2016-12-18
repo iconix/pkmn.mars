7e5f6aca88fc61e9a23d4db043193e0e3937d8b1
iconix <narho@microsoft.com>
2016-12-17 20:26:24 -0800

    Google Cloud Shell proxy service only permitted on ports 8080 to 8084

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 
 const app = require('./app');
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 8081;
 
 var server = app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
