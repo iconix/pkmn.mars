f9792b9c5e1c24dc5b44c897a8c8d57e80d42f2f
iconix <narho@microsoft.com>
2016-12-20 11:39:38 -0800

    Force https tweak tweak

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ app.set('trust proxy', true);
 
 // Always return the main index.html, so react-router render the route in the client
 app.get('*', (req, res) => {
-  if (req.protocol !== 'https') {
+  if (req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'] !== 'https') {
     res.redirect(301, 'https://' + req.host + req.originalUrl);
   } else {
     res.sendFile(path.resolve(__dirname, '..', 'target', 'index.html'));
