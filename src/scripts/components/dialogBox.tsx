789e9c0e06533a14ec37d350dbd8dc8a3e0dca5f
iconix <narho@microsoft.com>
2017-11-26 12:47:06 -0800

    Make dialogBox.tsx a presentational component

--- src/scripts/components/dialogBox.tsx
+++ src/scripts/components/dialogBox.tsx
@@ -13,36 +13,36 @@ interface DialogBoxProps {
     scene: Scene; // TODO: instead of passing around the entire scene, the React way for "bi-directional data flow" would be to pass down a helper function that lives on the Scene that will update data on the scene
 }
 
-export class DialogBox extends React.Component<DialogBoxProps, {}> {
-    private getReadyForTouch = (prevState: SceneState, props: SceneProps) => {
-        return { stage: prevState.stage, actionIndex: prevState.actionIndex, waitingForTouch: this.props.dialog.waitForTouchAfter }
+export function DialogBox(props: DialogBoxProps) {
+    // presentational component: functional, stateless
+
+    const getReadyForTouch = (prevState: SceneState): SceneState => {
+        return { stage: prevState.stage, actionIndex: prevState.actionIndex, waitingForTouch: props.dialog.waitForTouchAfter }
     };
 
-    private transitionEnterProps = {
+    const transitionEnterProps = {
         animation: "transition.slideLeftIn",
         style: {
             position: "relative",
             bottom: ""
         },
         duration: 500,
-        complete: () => { this.props.scene.setState(this.getReadyForTouch); }
+        complete: () => { props.scene.setState(getReadyForTouch); }
     };
 
-    render() {
-        const key = this.props.dialog.text;
-        const textElement = <DialogText text={key} />;
+    const key = props.dialog.text;
+    const textElement = <DialogText text={key} />;
 
-        // add a unique key prop to the element to animate,
-        // so that the transition group recognizes that the components are changing
-        // https://github.com/twitter-fabric/velocity-react/issues/29#issuecomment-148558624
-        const textToAnimate = React.cloneElement(textElement, { key });
+    // add a unique key prop to the element to animate,
+    // so that the transition group recognizes that the components are changing
+    // https://github.com/twitter-fabric/velocity-react/issues/29#issuecomment-148558624
+    const textToAnimate = React.cloneElement(textElement, { key });
 
-        return (
-            <div className={Constants.Classes.dialogBox}>
-                <VelocityTransitionGroup enter={this.transitionEnterProps} runOnMount>
-                    {textToAnimate}
-                </VelocityTransitionGroup>
-            </div>
-        );
-    }
+    return (
+        <div className={Constants.Classes.dialogBox}>
+            <VelocityTransitionGroup enter={transitionEnterProps} runOnMount>
+                {textToAnimate}
+            </VelocityTransitionGroup>
+        </div>
+    );
 }
